4e6c2746bacffb831114483271c957e2c6e05507
GUARACHI ARGUATA ALBERTH <alberth-code2@example.com>
2026-06-09 13:04:40 -0400

    Fix: cambiar sqlite a better-sqlite3

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,16 +8,23 @@ import { TvModule } from './tv.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
-      username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME || 'greenhome',
-      entities: [User, Tv, AccessLog],
-      synchronize: true,
-    }),
+    process.env.NODE_ENV === 'production'
+      ? TypeOrmModule.forRoot({
+          type: 'better-sqlite3',
+          database: ':memory:',
+          entities: [User, Tv, AccessLog],
+          synchronize: true,
+        })
+      : TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: process.env.DB_HOST || 'mysql',
+          port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+          username: process.env.DB_USER || 'root',
+          password: process.env.DB_PASSWORD || '',
+          database: process.env.DB_NAME || 'greenhome',
+          entities: [User, Tv, AccessLog],
+          synchronize: true,
+        }),
     AuthModule,
     TvModule,
   ],
